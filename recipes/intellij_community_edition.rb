995b091acca169d005c60d5997178798f4db95ea
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-04-25 22:38:48 -0700

    Update IntelliJ CE to 11.1.1

--- recipes/intellij_community_edition.rb
+++ recipes/intellij_community_edition.rb
@@ -1,5 +1,5 @@
-intellij_version = "10.5.2"
-pivotal_workstation_package "IntelliJ IDEA 10 CE" do
+intellij_version = "11.1.1"
+pivotal_workstation_package "IntelliJ IDEA 11 CE" do
   source "http://download.jetbrains.com/idea/ideaIC-#{intellij_version}.dmg"
   action :install
   checksum "69d7a7b055fcccb00695257385f585c1cb6e46eb29500184e0530b700f4c4ed5"
